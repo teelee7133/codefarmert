83e2177b19b411b88575eb2f4c74ce47b3e18b3c
Thomasina Lee <thomasina.lee@titanemail.com>
2020-03-05 13:32:54 +0000

    disable csp on script

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,11 +101,11 @@ module.exports = {
       options: {
         disableOnDev: true,
         reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeScriptHashes: false, // you can disable scripts sha256 hashes
         mergeStyleHashes: false, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self' www.google-analytics.com www.googletagmanager.com",
+          "script-src": "'self'  'unsafe-inline' www.google-analytics.com www.googletagmanager.com",
           "style-src": "'self' 'unsafe-inline'",
           "img-src": "'self' data: www.google-analytics.com"
           // you can add your directives or override defaults
