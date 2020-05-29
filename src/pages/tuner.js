a9c214f0f7224edc206da265644c1d65f489232c
T L <teelee7133@gmail.com>
2020-05-29 16:17:39 +0100

    initial tuner

---
+++ src/pages/tuner.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+import { Layout } from '../components/layout';
+import SEO from '../components/seo';
+import { WebTuner, summarize } from '../utils/webtuner';
+import tunerStyles from '../components/css/tuner.module.css';
+
+
+
+const Note = ({note}) => (
+  <div>
+    <div className={tunerStyles.noteName}>
+      {note.noteName}
+    </div>
+    <div className={tunerStyles.noteResidue}>
+      {note.weightedResidue.toFixed(2)}
+    </div>
+  </div>
+);
+
+const Tuner = () => {
+  const [active, setActive] = useState(0);
+  const [notes, setNotes] = useState([]);
+  const webtunerRef = useRef({});
+
+  const init = async () => {
+    const webtuner = new WebTuner();
+    await webtuner.init();
+    webtunerRef.current = {webtuner};
+  };
+
+  const close = async () => {
+    await webtunerRef.current.webtuner.close();
+    webtunerRef.current.webtuner = null;
+  };
+
+  const update = () => {
+    const webtuner = webtunerRef.current.webtuner;
+
+    if (!(active && webtuner)) {return;}
+    try {
+      const mynotes = summarize(webtuner.detectNotes());
+      setNotes(mynotes);
+      window.requestAnimationFrame(update);
+    } catch (e) {
+      setActive(0);
+      console.log(e);
+    }
+  };
+
+  useEffect( () => {
+    if (active && !webtunerRef.current.webtuner) {
+      (async () => {
+        try {
+          await init();
+          update();
+        } catch (e) {
+          setActive(0);
+          console.log(e);
+        }
+      })();
+    }
+    if (!active && webtunerRef.current.webtuner) {
+      close();
+    }
+    return () => {};
+  }, [active]);
+
+
+
+  return (
+    <div className={tunerStyles.outer}>
+
+      <div className={tunerStyles.displayArea} >
+        {notes.map((note, idx) => <Note key={idx} note={note} />)}
+      </div>
+      <button
+        onClick={() => setActive(active => !active)}
+        className={tunerStyles.activateButton}
+      >
+        {active ? 'Stop' : 'Start'}
+      </button>
+    </div>
+  );
+};
+
+
+const Index = () => {
+  return (
+    <Layout>
+      <SEO title='Tuner' />
+      <div>
+        <Tuner />
+      </div>
+    </Layout>
+  );
+};
+
+export default Index;
---
+++ src/utils/webtuner.js
@@ -0,0 +1,124 @@
+
+const signalFilter = (signal, weights, {output=null}) => {
+
+  const filtered = output || new Float64Array(signal.length - weights.length + 1);
+
+  filtered.forEach((value, index, array) => {
+    array[index] = signal
+      .slice(index, index + weights.length)
+      .reduce((accumulated, v, idx) => accumulated + v * weights[idx], 0);
+  });
+  return filtered;
+};
+
+const getMaximaIndices = (signal, {buffer=null}) => {
+  const d1 = signalFilter(signal, [-0.5, 0, 0.5], {output: buffer});
+  const maximas = [];
+  d1.forEach((value, index, array) => {
+    if (index <= 0 || index >= array.length) {return;}
+    if (d1[index] > 0 && d1[index + 1] < 0) {
+      maximas.push(
+        Math.abs(d1[index]) < Math.abs(d1[index + 1])
+          ? index
+          : index + 1 );
+    }
+  });
+  return maximas.map(x => x + 1);
+};
+
+const getNote = (freq)  => {
+  const C4 = 261.626;
+  const notes = [
+    'C', 'C#', 'D', 'Eb', 'E', 'F',
+    'Gb', 'G', 'Ab', 'A', 'Bb', 'B'
+  ];
+  const transformed = Math.log(freq / C4)/Math.log(2)* 12 + 48;
+  const semi = Math.round(transformed);
+  const quotient = Math.floor(semi/12);
+  const remainder = semi % 12;
+  return {
+    noteName: notes[remainder] ,
+    semitone: semi,
+    residue: transformed - semi,
+    octave: quotient,
+    noteValue: remainder,
+  };
+
+};
+
+export class WebTuner {
+
+  async init () {
+    this.stream = await navigator.mediaDevices.getUserMedia({audio: true});
+    this.audioCtx = new window.AudioContext();
+    this.source = this.audioCtx.createMediaStreamSource(this.stream);
+    this.analyser = this.audioCtx.createAnalyser();
+    this.analyser.minDecibels = -70;
+    this.source.connect(this.analyser);
+    this.data = {};
+    this.data.signal = new Uint8Array(this.analyser.frequencyBinCount);
+    this.data.smoothed = new Float64Array(this.analyser.frequencyBinCount - 4);
+    this.data.d1 = new Float64Array(this.analyser.frequencyBinCount - 6);
+  }
+
+  close () {
+    this.audioCtx.close();
+    this.stream.getTracks().forEach(track => track.stop());
+  }
+
+  async resume () {
+    return await this.audioCtx.resume();
+  }
+
+  async suspense() {
+    return await this.audioCtx.suspend();
+  }
+
+  getState () {
+    return this.audioCtx.state;
+  }
+
+  detectNotes () {
+    const C2 = 65;
+    const interval = this.audioCtx.sampleRate/ 2 / this.analyser.fftSize;
+    const signal = this.data.signal;
+    const smoothed = this.data.smoothed;
+    const d1 = this.data.d1;
+
+    this.analyser.getByteFrequencyData(signal);
+    signalFilter(
+      signal,
+      [0.1, 0.2, 0.4, 0.2, 0.1],
+      {output: smoothed}
+    );
+    const maxSignal = Math.max(...signal);
+
+    const maximaIndices = getMaximaIndices(smoothed, {buffer: d1}).map(x => x + 2);
+
+    const detectedNotes = maximaIndices
+      .map(x => Object.assign({x: x, freq: x * interval, decibel: signal[x]}, getNote(x * interval)))
+      .filter(obj => obj.freq >= C2 && obj.decibel > maxSignal * 0.25)
+      .sort((a, b) => b.decibel - a.decibel);
+
+    return detectedNotes;
+  }
+}
+
+
+export const summarize =  (detectedNotes) => {
+  const result = Object.entries(detectedNotes.reduce((res, note) => {
+    const noteName = note.noteName;
+    res[noteName] = (
+      res[noteName] ??
+      {noteName: noteName, count: 0, decibelTotal: 0, weightedResidue: 0}
+    );
+    res[noteName].count = res[noteName].count + 1;
+    res[noteName].decibelTotal = res[noteName].decibelTotal + note.decibel;
+    res[noteName].weightedResidue = res[noteName].weightedResidue + note.residue * note.decibel;
+    return res;
+  }, {}))
+    .map(([key, value]) => value)
+    .sort((a, b) => b.decibelTotal - a.decibelTotal);
+  result.forEach( x => x.weightedResidue = x.weightedResidue / x.decibelTotal);
+  return result;
+}
