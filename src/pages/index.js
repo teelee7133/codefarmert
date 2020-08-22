7e847566edad59591469ce32d7ccc882c5dc324f
T L <teelee7133@gmail.com>
2020-08-22 21:22:33 +0100

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Prism from 'prismjs';
 
 import { IndexLayout } from '../components/layout';
 import SEO from '../components/seo';
 import indexStyle from '../components/css/index.module.css';
+import 'prismjs/themes/prism-tomorrow.css';
 
-const Index = () => {
+const CodeSnippet = () => {
+  const code = `
+  let us, have, fun;
+  try {
+    const code = '';
+    while (have, fun);
+  } finally {
+    this;
+  }
+  `;
+  const html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
   return (
-    <IndexLayout>
-      <SEO title="Home" />
-      <div className={indexStyle.bannerContainer}>
-        <div className={indexStyle.banner}></div>
-      </div>
-      <div className={indexStyle.linkBlockContainer}>
-        <Link className={indexStyle.linkBlock} to="/blogs">
-          Blog
-        </Link>
-        <Link className={indexStyle.linkBlock} to="/mini-apps">
-          Mini Apps
-        </Link>
-        <Link className={indexStyle.linkBlock} to="/about">
-          About
-        </Link>
-      </div>
-    </IndexLayout>
+    <div className={indexStyle.codeSnippet}>
+      <pre>
+        <code dangerouslySetInnerHTML={{ __html: html }}></code>
+      </pre>
+    </div>
   );
 };
+
+const Index = () => (
+  <IndexLayout>
+    <SEO title="Home" />
+    <div className={indexStyle.bannerContainer}>
+      <CodeSnippet></CodeSnippet>
+    </div>
+    <div className={indexStyle.linkBlockContainer}>
+      <Link className={indexStyle.linkBlock} to="/blogs">
+        Blog
+      </Link>
+      <Link className={indexStyle.linkBlock} to="/mini-apps">
+        Mini Apps
+      </Link>
+      <Link className={indexStyle.linkBlock} to="/about">
+        About
+      </Link>
+    </div>
+  </IndexLayout>
+);
 
 export default Index;
